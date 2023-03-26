7197326f98ba4fd2669e3dcc8840ecd3b6c66c97
MaxianEdison <MaximilianEdison@gmail.com>
2023-03-26 14:01:33 +0330

    get all tasks

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
+
+type Task struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Done        bool   `json:"done"`
+}
 
 func main() {
 	// Load .env file and later use variables and credentials
@@ -32,20 +40,62 @@ func main() {
 	}
 	defer db.Close()
 
-	_, err = db.Exec(`
+	if _, err = db.Exec(`
 	CREATE TABLE IF NOT EXISTS tasks (
 		id INT PRIMARY KEY AUTO_INCREMENT,
 		title VARCHAR(255) NOT NULL,
 		description TEXT,
 		done BOOLEAN NOT NULL DEFAULT FALSE
-	)`)
-	if err != nil {
+	)`); err != nil {
 		log.Fatal(err)
 	}
 
-	pingErr := db.Ping()
-	if pingErr != nil {
-		log.Fatal(pingErr)
+	// pingErr := db.Ping()
+	// if pingErr != nil {
+	// 	log.Fatal(pingErr)
+	// }
+	// fmt.Println("Connected!")
+
+	// Register the HTTP handlers
+	http.HandleFunc("/api/tasks", getAllTasksHandler(db))
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func getAllTasksHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tasks, err := getAllTasks(db)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		writeJsonResponse(w, tasks)
 	}
-	fmt.Println("Connected!")
+}
+
+func getAllTasks(db *sql.DB) (tasks []Task, err error) {
+	rows, err := db.Query("SELECT id, title, description, done FROM tasks")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	tasks = make([]Task, 0)
+	for rows.Next() {
+		var task Task
+		if err := rows.Scan(&task.Id, &task.Title, &task.Description, &task.Done); err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, task)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+func writeJsonResponse(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
