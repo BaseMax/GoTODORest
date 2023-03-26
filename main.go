b463d6a9a9534c96d54a86c3f300775ba3eadd57
MaxianEdison <MaximilianEdison@gmail.com>
2023-03-26 14:57:38 +0330

    change some stuff

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
@@ -58,6 +62,7 @@ func main() {
 
 	// Register the HTTP handlers
 	http.HandleFunc("/api/tasks", getAllTasksHandler(db))
+	http.HandleFunc("/api/tasks/", taskHandler(db))
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
@@ -69,6 +74,71 @@ func getAllTasksHandler(db *sql.DB) http.HandlerFunc {
 			return
 		}
 		writeJsonResponse(w, tasks)
+	}
+}
+
+func taskHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		taskId, err := getTaskIdFromUrl(r.URL.Path)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		switch r.Method {
+		// Handle GET method
+		case http.MethodGet:
+			task, err := getTaskById(db, taskId)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			if task == nil {
+				http.NotFound(w, r)
+				return
+			}
+			writeJsonResponse(w, task)
+
+		// Handle PUT method
+		case http.MethodPut:
+			task, err := decodeTaskFromBody(r.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			task.Id = taskId
+			err = updateTask(db, task)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			writeJsonResponse(w, task)
+
+		// Handle POST method
+		case http.MethodPost:
+			task, err := decodeTaskFromBody(r.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			addTask(db, task)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			writeJsonResponse(w, task)
+
+		// Handke DELETE method
+		case http.MethodDelete:
+			err = deleteTask(db, taskId)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			fmt.Fprintf(w, "Task with ID %v deleted successfully\n", taskId)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
 	}
 }
 
@@ -98,4 +168,53 @@ func writeJsonResponse(w http.ResponseWriter, data interface{}) {
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func addTask(db *sql.DB, task *Task) error {
+	_, err := db.Exec("INSERT INTO tasks (title, description, done) VALUES ('?', '?', ?);", task.Title, task.Description, task.Done)
+	return err
+}
+
+func updateTask(db *sql.DB, task *Task) error {
+	_, err := db.Exec("UPDATE tasks SET title=?, description=?, done=? WHERE id=?", task.Title, task.Description, task.Done, task.Id)
+	return err
+}
+
+func deleteTask(db *sql.DB, id int) error {
+	_, err := db.Exec("DELETE FROM tasks WHERE id=?", id)
+	return err
+}
+
+func getTaskById(db *sql.DB, id int) (*Task, error) {
+	row := db.QueryRow("SELECT id, title, description, done FROM tasks WHERE id=?", id)
+	var task Task
+	if err := row.Scan(&task.Id, &task.Title, &task.Description, &task.Done); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &task, nil
+}
+
+func getTaskIdFromUrl(urlPath string) (int, error) {
+	re := regexp.MustCompile(`^/api/tasks/(\d+)$`)
+	matches := re.FindStringSubmatch(urlPath)
+	if matches == nil {
+		return 0, fmt.Errorf("%s", "Invalid task ID")
+	}
+	taskId, err := strconv.Atoi(matches[1])
+	if err != nil {
+		return 0, fmt.Errorf("%s", "Invalid task ID")
+	}
+	return taskId, nil
+}
+
+func decodeTaskFromBody(body io.ReadCloser) (*Task, error) {
+	defer body.Close()
+	var task Task
+	if err := json.NewDecoder(body).Decode(&task); err != nil {
+		return nil, err
+	}
+	return &task, nil
 }
